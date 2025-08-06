78822e9b9c0ed7d3de27e3341d336cd77f8a8737
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-06 18:49:17 +0300

    feat: implement services page with header and footer

---
+++ src/pages/ServicesPage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Services from "../components/Services"
+import Footer from "../components/Footer"
+
+const ServicesPage = () => {
+  return(
+    <>
+    <Header/>
+    <Services/>
+    <Footer/>
+    </>
+  )
+}
+
+export default ServicesPage
