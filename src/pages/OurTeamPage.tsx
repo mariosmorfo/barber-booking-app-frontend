898d25326cd42f964e7a57935d53b6cda226bf88
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-06 18:49:36 +0300

    feat: implement team page with header and footer

---
+++ src/pages/OurTeamPage.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import TeamSection from "../components/TeamSection"
+import Header from "../components/Header"
+
+const OurTeamPage = () => {
+  return(
+    <>
+    <Header/>
+    <TeamSection/>
+    <Footer/>
+    </>
+  )
+}
+
+export default OurTeamPage
