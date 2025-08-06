84e7d58ad175923b3d1b8ccb43fa7a93a388e657
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-06 18:45:49 +0300

    feat: import routes for navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import ServicesPage from "./pages/ServicesPage"
+import OurTeamPage from "./pages/OurTeamPage"
 
 
 function App() {
   return (
     <>
     <BrowserRouter>
-    <HomePage/>
+    <Routes>
+       
+       <Route path="/" element={<HomePage/>}/>
+       <Route path="/services" element={<ServicesPage/>}/>
+       <Route path="/team" element={<OurTeamPage/>}/>
+    
+    </Routes>
     </BrowserRouter>
+
     </>
   )
 }
