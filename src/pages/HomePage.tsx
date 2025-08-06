907061dea36f87b5d151a9d47388e2125fbf46f9
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-06 13:02:25 +0300

    feat: HomePage with Header, MainSection and Footer created

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import MainSection from "../components/MainSection";
+import Header from "../components/Header"
+
+const HomePage =  () => {
+  return (
+    <>
+    <Header/>
+    <MainSection/>
+    <Footer/>
+    </>
+  )
+}
+
+export default HomePage;
