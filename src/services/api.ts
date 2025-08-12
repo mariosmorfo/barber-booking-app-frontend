859e9ee358c41f05641593fb6b1955071727de58
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-12 18:31:46 +0300

    feat: add axios request/response interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
+import { getToken, clearToken } from "../utils/authTokenUtil";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: false,
-})
+  withCredentials: false, 
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getToken();           
+    if (token) {
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `Bearer ${token}`; 
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      clearToken?.();
+    }
+    return Promise.reject(err);
+  }
+);
