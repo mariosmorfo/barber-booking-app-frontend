7b2ba2d6dd2640adaaf44c94b7159c2f4b898cb7
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-15 18:39:01 +0300

    feat: add StaffHeader for Admin/Barber and CustomerHeader for Customenrs

--- src/components/HeaderWrapper.tsx
+++ src/components/HeaderWrapper.tsx
@@ -1,14 +1,12 @@
 import { useAuth } from "../context/AuthContext";
-import AdminHeader from "./AdminHeader";
+import StaffHeader from "./StaffHeader";
 import CustomerHeader from "./CustomerHeader";
 
-const HeaderWrapper = () => {
+export default function HeaderWrapper() {
   const { role } = useAuth();
 
-  if (role === "ADMIN") {
-    return <AdminHeader />;
+  if (role === "ADMIN" || role === "BARBER") {
+    return <StaffHeader/>;
   }
-  return <CustomerHeader />;
-};
-
-export default HeaderWrapper;
+  return <CustomerHeader/>;
+}
